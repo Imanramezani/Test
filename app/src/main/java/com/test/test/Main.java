8c9b44055a10504b7a3f36e01f7fdaa60202e745
iman <imanramezani@yahoo.com>
2018-01-12 17:48:27 +0330

    Add Toast to project

--- app/src/main/java/com/test/test/Main.java
+++ app/src/main/java/com/test/test/Main.java
@@ -10,6 +10,7 @@ public class Main extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toast.makeText(this, "Hello GitHub", Toast.LENGTH_SHORT).show();
+//        Toast.makeText(this, "Hello GitHub", Toast.LENGTH_SHORT).show();
+
     }
 }
